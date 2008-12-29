7e487a55d9cfaaf78dd4ce1891b34a7ce401bf11
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2008-12-29 16:57:49 +0200

    GeSHi updated to 1.0.8.2

--- lib/geshi/ruby.php
+++ lib/geshi/ruby.php
@@ -4,10 +4,17 @@
  * --------
  * Author: Moises Deniz
  * Copyright: (c) 2007 Moises Deniz
- * Release Version: 1.0.7.19
+ * Release Version: 1.0.8.2
  * Date Started: 2007/03/21
  *
- * Ruby language file for GeSHi
+ * Ruby language file for GeSHi.
+ *
+ * CHANGES
+ * -------
+ * 2008/05/23 (1.0.7.22)
+ *   -  Added description of extra language features (SF#1970248)
+ * 2007/03/21 (1.0.7.19)
+ *   -  Initial release
  *
  *************************************************************************************
  *
@@ -46,7 +53,7 @@ $language_data = array (
                 'until', 'when', 'BEGIN', 'END', 'include'
             ),
         2 => array(
-                '__FILE__', '__LINE__', 'false', 'nil', 'self', 'true', 
+                '__FILE__', '__LINE__', 'false', 'nil', 'self', 'true',
                 'return'
             ),
         3 => array(
@@ -54,74 +61,73 @@ $language_data = array (
                 'autoload', 'binding', 'caller', 'catch', 'chop', 'chop!',
                 'chomp', 'chomp!', 'eval', 'exec', 'exit', 'exit!', 'fail',
                 'fork', 'format', 'gets', 'global_variables', 'gsub', 'gsub!',
-                'iterator?', 'lambda', 'load', 'local_variables', 'loop', 
-                'open', 'p', 'print', 'printf', 'proc', 'putc', 'puts', 
-                'raise', 'rand', 'readline', 'readlines', 'require', 'select', 
+                'iterator?', 'lambda', 'load', 'local_variables', 'loop',
+                'open', 'p', 'print', 'printf', 'proc', 'putc', 'puts',
+                'raise', 'rand', 'readline', 'readlines', 'require', 'select',
                 'sleep', 'split', 'sprintf', 'srand', 'sub', 'sub!', 'syscall',
                 'system', 'trace_var', 'trap', 'untrace_var'
             ),
         4 => array(
-                'Abbrev', 'ArgumentError', 'Base64', 'Benchmark', 
-                'Benchmark::Tms', 'Bignum', 'Binding', 'CGI', 'CGI::Cookie', 
+                'Abbrev', 'ArgumentError', 'Base64', 'Benchmark',
+                'Benchmark::Tms', 'Bignum', 'Binding', 'CGI', 'CGI::Cookie',
                 'CGI::HtmlExtension', 'CGI::QueryExtension',
-                'CGI::Session', 'CGI::Session::FileStore', 
-                'CGI::Session::MemoryStore', 'Class', 'Comparable', 'Complex', 
+                'CGI::Session', 'CGI::Session::FileStore',
+                'CGI::Session::MemoryStore', 'Class', 'Comparable', 'Complex',
                 'ConditionVariable', 'Continuation', 'Data',
-                'Date', 'DateTime', 'Delegator', 'Dir', 'EOFError', 'ERB', 
-                'ERB::Util', 'Enumerable', 'Enumerable::Enumerator', 'Errno', 
+                'Date', 'DateTime', 'Delegator', 'Dir', 'EOFError', 'ERB',
+                'ERB::Util', 'Enumerable', 'Enumerable::Enumerator', 'Errno',
                 'Exception', 'FalseClass', 'File',
-                'File::Constants', 'File::Stat', 'FileTest', 'FileUtils', 
-                'FileUtils::DryRun', 'FileUtils::NoWrite', 
+                'File::Constants', 'File::Stat', 'FileTest', 'FileUtils',
+                'FileUtils::DryRun', 'FileUtils::NoWrite',
                 'FileUtils::StreamUtils_', 'FileUtils::Verbose', 'Find',
-                'Fixnum', 'FloatDomainError', 'Forwardable', 'GC', 'Generator', 
-                'Hash', 'IO', 'IOError', 'Iconv', 'Iconv::BrokenLibrary', 
+                'Fixnum', 'FloatDomainError', 'Forwardable', 'GC', 'Generator',
+                'Hash', 'IO', 'IOError', 'Iconv', 'Iconv::BrokenLibrary',
                 'Iconv::Failure', 'Iconv::IllegalSequence',
-                'Iconv::InvalidCharacter', 'Iconv::InvalidEncoding', 
-                'Iconv::OutOfRange', 'IndexError', 'Interrupt', 'Kernel', 
+                'Iconv::InvalidCharacter', 'Iconv::InvalidEncoding',
+                'Iconv::OutOfRange', 'IndexError', 'Interrupt', 'Kernel',
                 'LoadError', 'LocalJumpError', 'Logger', 'Logger::Application',
-                'Logger::Error', 'Logger::Formatter', 'Logger::LogDevice', 
-                'Logger::LogDevice::LogDeviceMutex', 'Logger::Severity', 
+                'Logger::Error', 'Logger::Formatter', 'Logger::LogDevice',
+                'Logger::LogDevice::LogDeviceMutex', 'Logger::Severity',
                 'Logger::ShiftingError', 'Marshal', 'MatchData',
-                'Math', 'Matrix', 'Method', 'Module', 'Mutex', 'NameError', 
-                'NameError::message', 'NilClass', 'NoMemoryError', 
+                'Math', 'Matrix', 'Method', 'Module', 'Mutex', 'NameError',
+                'NameError::message', 'NilClass', 'NoMemoryError',
                 'NoMethodError', 'NotImplementedError', 'Numeric', 'Object',
-                'ObjectSpace', 'Observable', 'PStore', 'PStore::Error', 
-                'Pathname', 'Precision', 'Proc', 'Process', 'Process::GID', 
+                'ObjectSpace', 'Observable', 'PStore', 'PStore::Error',
+                'Pathname', 'Precision', 'Proc', 'Process', 'Process::GID',
                 'Process::Status', 'Process::Sys', 'Process::UID', 'Queue',
-                'Range', 'RangeError', 'Rational', 'Regexp', 'RegexpError', 
-                'RuntimeError', 'ScriptError', 'SecurityError', 'Set', 
+                'Range', 'RangeError', 'Rational', 'Regexp', 'RegexpError',
+                'RuntimeError', 'ScriptError', 'SecurityError', 'Set',
                 'Shellwords', 'Signal', 'SignalException', 'SimpleDelegator',
-                'SingleForwardable', 'Singleton', 'SingletonClassMethods', 
-                'SizedQueue', 'SortedSet', 'StandardError', 'StringIO', 
+                'SingleForwardable', 'Singleton', 'SingletonClassMethods',
+                'SizedQueue', 'SortedSet', 'StandardError', 'StringIO',
                 'StringScanner', 'StringScanner::Error', 'Struct', 'Symbol',
-                'SyncEnumerator', 'SyntaxError', 'SystemCallError', 
-                'SystemExit', 'SystemStackError', 'Tempfile', 
-                'Test::Unit::TestCase', 'Test::Unit', 'Test', 'Thread', 
+                'SyncEnumerator', 'SyntaxError', 'SystemCallError',
+                'SystemExit', 'SystemStackError', 'Tempfile',
+                'Test::Unit::TestCase', 'Test::Unit', 'Test', 'Thread',
                 'ThreadError', 'ThreadGroup',
-                'ThreadsWait', 'Time', 'TrueClass', 'TypeError', 'URI', 
-                'URI::BadURIError', 'URI::Error', 'URI::Escape', 'URI::FTP', 
+                'ThreadsWait', 'Time', 'TrueClass', 'TypeError', 'URI',
+                'URI::BadURIError', 'URI::Error', 'URI::Escape', 'URI::FTP',
                 'URI::Generic', 'URI::HTTP', 'URI::HTTPS',
-                'URI::InvalidComponentError', 'URI::InvalidURIError', 
-                'URI::LDAP', 'URI::MailTo', 'URI::REGEXP', 
-                'URI::REGEXP::PATTERN', 'UnboundMethod', 'Vector', 'YAML', 
+                'URI::InvalidComponentError', 'URI::InvalidURIError',
+                'URI::LDAP', 'URI::MailTo', 'URI::REGEXP',
+                'URI::REGEXP::PATTERN', 'UnboundMethod', 'Vector', 'YAML',
                 'ZeroDivisionError', 'Zlib',
-                'Zlib::BufError', 'Zlib::DataError', 'Zlib::Deflate', 
-                'Zlib::Error', 'Zlib::GzipFile', 'Zlib::GzipFile::CRCError', 
+                'Zlib::BufError', 'Zlib::DataError', 'Zlib::Deflate',
+                'Zlib::Error', 'Zlib::GzipFile', 'Zlib::GzipFile::CRCError',
                 'Zlib::GzipFile::Error', 'Zlib::GzipFile::LengthError',
-                'Zlib::GzipFile::NoFooter', 'Zlib::GzipReader', 
-                'Zlib::GzipWriter', 'Zlib::Inflate', 'Zlib::MemError', 
-                'Zlib::NeedDict', 'Zlib::StreamEnd', 'Zlib::StreamError', 
+                'Zlib::GzipFile::NoFooter', 'Zlib::GzipReader',
+                'Zlib::GzipWriter', 'Zlib::Inflate', 'Zlib::MemError',
+                'Zlib::NeedDict', 'Zlib::StreamEnd', 'Zlib::StreamError',
                 'Zlib::VersionError',
                 'Zlib::ZStream',
-                'Enumerable',
-                'HTML::Selector', 'HashWithIndifferentAccess', 'Inflector', 
-                'Inflector::Inflections', 'Mime', 'Mime::Type', 
-                'OCI8AutoRecover', 'Symbol', 'TimeZone', 'XmlSimple'
+                'HTML::Selector', 'HashWithIndifferentAccess', 'Inflector',
+                'Inflector::Inflections', 'Mime', 'Mime::Type',
+                'OCI8AutoRecover', 'TimeZone', 'XmlSimple'
             ),
         ),
     'SYMBOLS' => array(
         '(', ')', '[', ']', '{', '}', '%', '&', '*', '|', '/', '<', '>',
-        '+', '-', '=&gt;', '=>', '<<'
+        '+', '-', '=>', '<<'
         ),
     'CASE_SENSITIVE' => array(
         GESHI_COMMENTS => false,
@@ -139,7 +145,7 @@ $language_data = array (
             ),
         'COMMENTS' => array(
             1 => 'color:#008000; font-style:italic;',
-                    'MULTI' => 'color:#000080; font-style:italic;'
+            'MULTI' => 'color:#000080; font-style:italic;'
             ),
         'ESCAPE_CHAR' => array(
             0 => 'color:#000099;'
@@ -174,28 +180,29 @@ $language_data = array (
     'URLS' => array(
         1 => '',
         2 => '',
-        3 => ''
+        3 => '',
+        4 => ''
         ),
     'OOLANG' => true,
     'OBJECT_SPLITTERS' => array(
         1 => '.'
         ),
     'REGEXPS' => array(
-        0 => array(
+        0 => array(//Variables
             GESHI_SEARCH => "([[:space:]])(\\$[a-zA-Z_][a-zA-Z0-9_]*)",
             GESHI_REPLACE => '\\2',
             GESHI_MODIFIERS => '',
             GESHI_BEFORE => '\\1',
             GESHI_AFTER => ''
             ),
-        1 => array(
+        1 => array(//Arrays
             GESHI_SEARCH => "([[:space:]])(@[a-zA-Z_][a-zA-Z0-9_]*)",
             GESHI_REPLACE => '\\2',
             GESHI_MODIFIERS => '',
             GESHI_BEFORE => '\\1',
             GESHI_AFTER => ''
             ),
-        2 => "([A-Z][a-zA-Z0-9_]*::)+[A-Z][a-zA-Z0-9_]*",
+        2 => "([A-Z][a-zA-Z0-9_]*::)+[A-Z][a-zA-Z0-9_]*",//Static OOP symbols
         3 => array(
             GESHI_SEARCH => "([[:space:]]|\[|\()(:[a-zA-Z_][a-zA-Z0-9_]*)",
             GESHI_REPLACE => '\\2',
@@ -212,7 +219,8 @@ $language_data = array (
         ),
     'HIGHLIGHT_STRICT_BLOCK' => array(
         0 => true,
-        )
+        ),
+    'TAB_WIDTH' => 2
 );
 
 ?>
