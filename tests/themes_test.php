5e0a6816ae76e0701175ec44fd84949b2332c9d6
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2010-03-23 15:33:41 +0000

    Some tests added
    git-svn-id: https://plugins.svn.wordpress.org/codecolorer/trunk@220946 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tests/themes_test.php
@@ -0,0 +1,25 @@
+<?php
+/*
+CodeColorer plugin unit tests
+http://kpumuk.info/projects/wordpress-plugins/codecolorer
+*/
+
+require_once 'test_helper.php';
+
+class ThemesTest extends PHPUnit_Framework_TestCase {
+  public function testTextWithoutCode() {
+    $this->assertEquals('hello, world', codecolorer_highlight('hello, world'));
+  }
+  
+  public function testTextWithCodeNoLanguage() {
+    $this->assertEquals("\n\n<div class=\"codecolorer-container text default\" style=\"overflow:auto;white-space:nowrap;width:435px;\"><div class=\"text codecolorer\">hello, world</div></div>\n\n", codecolorer_highlight('[cc]hello, world[/cc]'));
+  }
+
+  public function testTextWithCodeTextLanguage() {
+    $this->assertEquals("\n\n<div class=\"codecolorer-container text default\" style=\"overflow:auto;white-space:nowrap;width:435px;\"><div class=\"text codecolorer\">hello, world</div></div>\n\n", codecolorer_highlight('[cc lang="text"]hello, world[/cc]'));
+  }
+
+  public function testTextWithCodePHPLanguage() {
+    $this->assertEquals("\n\n<div class=\"codecolorer-container php default\" style=\"overflow:auto;white-space:nowrap;width:435px;\"><div class=\"php codecolorer\"><span class=\"re0\">\$a</span> <span class=\"sy0\">=</span> <span class=\"nu0\">10</span><span class=\"sy0\">;</span></div></div>\n\n", codecolorer_highlight('[cc lang="php"]$a = 10;[/cc]'));
+  }
+}
